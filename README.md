7ab4deb939dfe330a27d09ea5ab0831d5383e990
acceleraterd <2516508803@qq.com>
2022-07-20 14:25:44 +0800

    first
    first

---
+++ README.md
@@ -0,0 +1,45 @@
+# bubble清单
+
+一个基于gin+gorm开发的练手小项目，通过该项目初识go web开发该有的姿势。
+
+前端源码地址：[bubble_frontend](https://github.com/Q1mi/bubble_frontend)，是基于vue和ElementUI开发
+
+## 使用指南
+
+### 配置MySQL
+1. 在你的数据库中执行以下命令，创建本项目所用的数据库：
+```sql
+CREATE DATABASE bubble DEFAULT CHARSET=utf8mb4;
+```
+2. 在`bubble/conf/config.ini`文件中按如下提示配置数据库连接信息。
+
+```ini
+port = 9000
+release = false
+
+[mysql]
+user = 数据库用户名
+password = 数据库密码
+host = 数据库host地址
+port = 数据库端口
+db = bubble
+```
+
+### 编译
+```bash
+go build
+```
+
+### 执行
+
+Mac/Unix：
+```bash
+./bubble conf/config.ini
+```
+Windows:
+```bash
+./bubble.exe conf/config.ini
+```
+
+启动之后，使用浏览器打开`http://127.0.0.1:9000/`即可。
+
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bubble/dao"
+	"bubble/models"
+	"bubble/routers"
+	"bubble/setting"
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage：./bubble conf/config.ini")
+		return
+	}
+	// 加载配置文件
+	if err := setting.Init(os.Args[1]); err != nil {
+		fmt.Printf("load config from file failed, err:%v\n", err)
+		return
+	}
+	// 创建数据库
+	// sql: CREATE DATABASE bubble;
+	// 连接数据库
+	err := dao.InitMySQL(setting.Conf.MySQLConfig)
+	if err != nil {
+		fmt.Printf("init mysql failed, err:%v\n", err)
+		return
+	}
+	defer dao.Close() // 程序退出关闭数据库连接
+	// Automigrate()操作，用于刷新数据库中的表，使其保持最新。即让数据库之前存储的记录的表字段和程序中最新使用的表字段保持一致（只增不减）
+	dao.DB.AutoMigrate(&models.Todo{})
+	// 注册路由
+	r := routers.SetupRouter()
+	if err := r.Run(fmt.Sprintf(":%d", setting.Conf.Port)); err != nil {
+		fmt.Printf("server startup failed, err:%v\n", err)
+	}
+}
